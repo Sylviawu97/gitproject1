cdd0b7853bd7e6e523b8ca79ac5f2e3b0ad76cff
Meixuan Wu <mwu@LABA.PSWAS.LOCAL>
2023-03-31 15:27:31 -0700

    greet

---
+++ functionBigLab.js
@@ -0,0 +1,7 @@
+//greetCustomer that will log "Welcome to the store" + the name parameter
+
+const greetCustomer =(name)=>{
+    console.log("Welcome to the store "+ name)
+}
+greetCustomer(Sylvia)
+
